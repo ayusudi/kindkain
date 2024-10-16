fa093f3b415c9533ac5cc74f5d533fad71e354be
Ayu Sudi <ayusudi.abc@gmail.com>
2024-10-16 15:16:46 +0700

    update typo and link

--- src/app/partnership/page.jsx
+++ src/app/partnership/page.jsx
@@ -1,40 +1,88 @@
-"use client"
-import { useTranslations } from 'next-intl';
-import Image from 'next/image';
+"use client";
+import { useTranslations } from "next-intl";
+import Image from "next/image";
 
 const CardComponent = ({ title, subtitle, button, buttonTo }) => {
   return (
-    <div className='flex flex-col gap-4 md:gap-5 items-start'>
-      <p style={{ whiteSpace: 'pre-wrap', textTransform: 'capitalize' }} className='text-darktext font-quicksand font-bold text-xl md:text-4xl'>{title}</p>
-      <p className='text-gray font-nunito text-base md:text-2xl' style={{ whiteSpace: "pre-wrap" }}>{subtitle}</p>
-      <a href={buttonTo} className='rounded-full py-2 md:py-3 px-5 md:px-6 text-yellow text-base md:text-2xl bg-darkpurple font-quicksand font-semibold'>{button}</a>
+    <div className="flex flex-col gap-4 md:gap-5 items-start">
+      <p
+        style={{ whiteSpace: "pre-wrap", textTransform: "capitalize" }}
+        className="text-darktext font-quicksand font-bold text-xl md:text-4xl"
+      >
+        {title}
+      </p>
+      <p
+        className="text-gray font-nunito text-base md:text-2xl"
+        style={{ whiteSpace: "pre-wrap" }}
+      >
+        {subtitle}
+      </p>
+      <a
+        href={buttonTo}
+        className="rounded-full py-2 md:py-3 px-5 md:px-6 text-yellow text-base md:text-2xl bg-darkpurple font-quicksand font-semibold"
+      >
+        {button}
+      </a>
     </div>
-  )
-}
+  );
+};
 export default function HomePage() {
-  const t = useTranslations('PartnershipPage');
+  const t = useTranslations("PartnershipPage");
 
   return (
-    <section className='bg-cream'>
-      <div className='max-w-screen-2xl m-auto'>
-        <div className='flex justify-center flex-col md:px-16 md:pb-20 gap-2 md:gap-6'>
+    <section className="bg-cream">
+      <div className="max-w-screen-2xl m-auto">
+        <div className="flex justify-center flex-col md:px-16 md:pb-20 gap-2 md:gap-6">
           {/* <Image alt='grey-image' src="/product-warranty.png" width="300" height="300" className='w-full' /> */}
-          <div className='w-11/12 md:w-full py-8 mb-12 md:mb-24 text-[24px] m-auto md:px-10 flex flex-col gap-10 md:gap-24 text-gray font-nunito'>
+          <div className="w-11/12 md:w-full py-8 mb-12 md:mb-24 text-[24px] m-auto md:px-10 flex flex-col gap-10 md:gap-24 text-gray font-nunito">
             {/* <div> */}
-            <p style={{ lineHeight: "120%", textTransform: "capitalize", whiteSpace: "pre-wrap" }} className='font-quicksand font-bold text-darkpurple text-3xl md:text-[56px]'>Partnership</p>
+            <p
+              style={{
+                lineHeight: "120%",
+                textTransform: "capitalize",
+                whiteSpace: "pre-wrap",
+              }}
+              className="font-quicksand font-bold text-darkpurple text-3xl md:text-[56px]"
+            >
+              Partnership
+            </p>
             {/* <Image alt='grey-image-alt' src="/product-warranty.png" width="300" height="300" className='mt-4 mt:mb-10 w-full rounded-lg' /> */}
             {/* </div> */}
-            <CardComponent button={t('b1')} buttonTo={'https://wa.me/628111329911'} title={t('q1')} subtitle={t('a1')} />
-            <div className='flex flex-col gap-5 items-start'>
-              <p style={{ whiteSpace: 'pre-wrap', textTransform: 'capitalize' }} className='text-darktext font-quicksand font-bold text-xl md:text-4xl'>{t('q2')}</p>
-              <p className='text-base md:text-2xl' style={{ whiteSpace: "pre-wrap" }}>{t('a2')}</p>
+            <CardComponent
+              button={t("b1")}
+              buttonTo={"https://forms.gle/YnZpkfdea9me5bTz5"}
+              title={t("q1")}
+              subtitle={t("a1")}
+            />
+            <div className="flex flex-col gap-5 items-start">
+              <p
+                style={{ whiteSpace: "pre-wrap", textTransform: "capitalize" }}
+                className="text-darktext font-quicksand font-bold text-xl md:text-4xl"
+              >
+                {t("q2")}
+              </p>
+              <p
+                className="text-base md:text-2xl"
+                style={{ whiteSpace: "pre-wrap" }}
+              >
+                {t("a2")}
+              </p>
             </div>
-            <CardComponent button={t('b3')} buttonTo={'https://wa.me/628111329911'} title={t('q3')} subtitle={t('a3')} />
-            <CardComponent button={t('b4')} buttonTo={'https://wa.me/628111329911'} title={t('q4')} subtitle={t('a4')} />
+            <CardComponent
+              button={t("b3")}
+              buttonTo={"https://wa.me/628111329911"}
+              title={t("q3")}
+              subtitle={t("a3")}
+            />
+            <CardComponent
+              button={t("b4")}
+              buttonTo={"https://wa.me/628111329911"}
+              title={t("q4")}
+              subtitle={t("a4")}
+            />
           </div>
         </div>
       </div>
-
     </section>
-  )
+  );
 }
--- src/components/components_productpage/variants.jsx
+++ src/components/components_productpage/variants.jsx
@@ -1,38 +1,68 @@
-"use client"
-import { useEffect, useState } from "react"
+"use client";
+import { useState } from "react";
 
 export default function Component({ variants, product, setProduct }) {
   const listnya = () => {
-    let keys = Object.keys(variants)
-    let array = []
-    keys.forEach(el => {
-      array.push({ ...variants[el], name: el, status: false, },)
-    })
-    array[0].status = true
-    return array
-  }
-  const [list, setList] = useState(listnya())
+    let keys = Object.keys(variants);
+    let array = [];
+    keys.forEach((el) => {
+      array.push({ ...variants[el], name: el, status: false });
+    });
+    array[0].status = true;
+    return array;
+  };
+  const [list, setList] = useState(listnya());
   const change = (i) => {
-    let temp = [...list]
-    let { photo, shopee, tokopedia, lazada, tiktok, price } = product.variants[temp[i].name]
-    price = 'IDR ' + price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-    let tempProduct = { ...product, photo, shopee, tokopedia, lazada, tiktok, price }
-    temp.forEach(el => el.status = false)
-    temp[i].status = true
-    setProduct(tempProduct)
-    setList(temp)
-  }
+    let temp = [...list];
+    let { photo, shopee, tokopedia, lazada, tiktok, price } =
+      product.variants[temp[i].name];
+    price = "IDR " + price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    let tempProduct = {
+      ...product,
+      photo,
+      shopee,
+      tokopedia,
+      lazada,
+      tiktok,
+      price,
+    };
+    temp.forEach((el) => (el.status = false));
+    temp[i].status = true;
+    setProduct(tempProduct);
+    setList(temp);
+  };
   return (
     <div>
-      <p className='text-base md:text-[20px] text-darktext font-nunito font-semibold mb-3'>Variants Available</p>
-      <div className='flex flex-wrap gap-3'>
-        {
-          list.map((el, i) => {
-            if (el.status) return <div key={i} className='flex p-2 md:py-2.5 md:px-3 border rounded-full border-darkpurple cursor-pointer'><p className='text-base md:text-[20px] font-nunito font-bold text-darkpurple'>{el.name}</p></div>
-            else return <div key={i} onClick={() => change(i)} className='p-2 md:py-2.5 md:px-3 border rounded-full border-gray cursor-pointer'><p className='text-base md:text-[20px] font-nunito font-bold text-gray'>{el.name}</p></div>
-          })
-        }
+      <p className="text-base md:text-[20px] text-darktext font-nunito font-semibold mb-3">
+        Variants Available
+      </p>
+      <div className="flex flex-wrap gap-3">
+        {list.map((el, i) => {
+          if (el.status)
+            return (
+              <div
+                key={i}
+                className="flex p-2 md:py-2.5 md:px-3 border rounded-full border-darkpurple cursor-pointer"
+              >
+                <p className="text-base md:text-[20px] font-nunito font-bold text-darkpurple">
+                  {el.name}
+                </p>
+              </div>
+            );
+          else
+            return (
+              <div
+                key={i}
+                onClick={() => change(i)}
+                className="p-2 md:py-2.5 md:px-3 border rounded-full border-gray cursor-pointer"
+              >
+                <p className="text-base md:text-[20px] font-nunito font-bold text-gray">
+                  {el.name}
+                </p>
+              </div>
+            );
+        })}
       </div>
     </div>
-  )
+  );
 }
